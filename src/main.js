2d6530e88f2acf6e74c386c849b6eb02bad9de06
shilpa <mulgeshilpa@gmail.com>
2023-05-19 22:29:51 +0530

    first commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+createApp(App).mount("#app");
