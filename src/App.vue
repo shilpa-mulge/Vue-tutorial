21a78712dc219afbf2cde5f829f48bdd59b44681
shilpa <mulgeshilpa@gmail.com>
2023-05-20 07:59:05 +0530

    input field

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
-<script setup></script>
+<script></script>
 
-<template></template>
+<template>
+  <div class="container">
+    <h2 class="mt-5 text-center">My todo App</h2>
+  </div>
+  <div class="d-flex">
+    <input
+      v-model="task"
+      type="text"
+      placeholder="enter task"
+      class="form-control"
+    />
+    <button @click="submitTask" class="btn btn-warning rounded-0">
+      submit
+    </button>
+  </div>
+</template>
 
 <style scoped></style>
