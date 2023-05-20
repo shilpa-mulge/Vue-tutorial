b3f9acb3deb5a5bbde0a25855441bcb5f157f673
shilpa <mulgeshilpa@gmail.com>
2023-05-20 08:02:14 +0530

    task table created

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,21 @@
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      tasks: [
+        {
+          name: "read book",
+          status: "to-do",
+        },
+        {
+          name: "play football",
+          status: "in-progress",
+        },
+      ],
+    };
+  },
+};
+</script>
 
 <template>
   <div class="container">
@@ -11,10 +28,34 @@
       placeholder="enter task"
       class="form-control"
     />
-    <button @click="submitTask" class="btn btn-warning rounded-0">
-      submit
-    </button>
+    <button class="btn btn-warning rounded-0">submit</button>
   </div>
+  <table class="table table-dark mt-5 w-100">
+    <thead>
+      <tr>
+        <th scope="col">Task</th>
+        <th scope="col">Status</th>
+        <th scope="col">#</th>
+        <th scope="col">#</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(task, index) in tasks" :key="index">
+        <td>{{ task.name }}</td>
+        <td>{{ task.status }}</td>
+        <td>
+          <div class="text-center">
+            <span class="fa fa-pen"></span>
+          </div>
+        </td>
+        <td>
+          <div class="text-center">
+            <span class="fa fa-trash"></span>
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <style scoped></style>
